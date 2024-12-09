906ba33f8bb43b5ed1757821287f7e6ea7114d98
TheoNDionne <theo.nathaniel.dionne@usherbrooke.ca>
2024-12-08 21:01:53 -0500

    Transfered content to notebook instead

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # GroupTheoryHomework
+
+Some code to do the homework for Applications of Group Theory in Condensed Matter Physics, given by Maia GV autumn 2024.
--- irreps_and_decompositions.py
+++ irreps_and_decompositions.py
@@ -2,6 +2,8 @@
 
 A simple script that uses the character table for a group and the characters of one of its representations to determine
 the rep's decomposition.
+
+ADD OTHER STUFF
 """
 
 # imports
@@ -24,6 +26,31 @@ def get_irrep_multiplicity(representation_characters, character_table, elements_
 # main body of the script
 if __name__ == "__main__":
 
+############## AB4 (PG: C4v)
+
+    # define the character table of the PG
+    character_table_C4v = np.array([
+        [1, 1, 1, 1, 1],
+        [1, 1, 1,-1,-1],
+        [1, 1,-1, 1,-1],
+        [1, 1,-1,-1, 1],
+        [2,-2, 0, 0, 0]
+    ])  
+
+    # define the number of elements per class
+    elements_per_class_C4v = np.array([1,1,2,2,2])
+
+    # the characters of the mechanical representation
+    M_characters_C4v = np.array([15,-1,1,1,3])
+
+    # Getting and printing the mutliplicities
+    M_multiplicities_C4v = get_irrep_multiplicity(M_characters_C4v, character_table_C4v, elements_per_class_C4v)
+    print(M_multiplicities_C4v)
+    
+    ### OUTPUT : [3. 1. 1. 2. 4.]
+
+############## UF6 (PG: Oh)
+
     # define the character table of the PG
     character_table_Oh = np.array([
         [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
@@ -39,13 +66,13 @@ if __name__ == "__main__":
     ])  
 
     # define the number of elements per class
-    elements_per_class_Oh = np.array([1,3,6,6,8,1,3,6,6,8])
+    elements_per_class_Oh = np.array([1, 3, 6, 6, 8, 1, 3, 6, 6, 8])
 
     # the characters of the mechanical representation
-    M_characters = np.array([21,-3,3,-1,0,-3,5,-1,3,0])
+    M_characters_Oh = np.array([21,-3, 3,-1, 0,-3, 5,-1, 3, 0])
 
     # Getting and printing the mutliplicities
-    M_multiplicities = get_irrep_multiplicity(M_characters, character_table_Oh, elements_per_class_Oh)
-    print(M_multiplicities)
+    M_multiplicities_Oh = get_irrep_multiplicity(M_characters_Oh, character_table_Oh, elements_per_class_Oh)
+    print(M_multiplicities_Oh)
     
     ### OUTPUT : [1. 0. 0. 0. 1. 0. 1. 3. 1. 1.]
